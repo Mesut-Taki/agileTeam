d96c6d9233087e83d9ebab2e9d0e6fc7b50b5178
kenan <kulkenan212@gmail.com>
2021-09-09 14:00:26 +0300

    V4

--- src/projectvendingmachine/Methods.java
+++ src/projectvendingmachine/Methods.java
@@ -1,19 +1,104 @@
 package projectvendingmachine;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Methods {
 
-    public double select(Products product) {
-        System.out.println(product.toString());
+
+    public double money(){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Ne kadar para yatirmak istersiniz?");
+        double money= scan.nextDouble();
+        return money;
+    }
+    public double select(Products products){
+        System.out.println(products.toString());
         Scanner scan=new Scanner(System.in);
         do {
-            System.out.println("lutfen almak istediginiz urunu giiriniz(1'den 10a mkadar)");
-            product.setProductNum(scan.nextInt());
-            if (product.getProductNum()<1||product.getProductNum()>10)
-                System.out.println("lutfen gecerli urun giriniz");
+            System.out.println("Lutfen almak istediginiz urunun numarasinin giriniz ( 1'den 10'a kadar)");
+            System.out.println("Cikis icin 0'a basiniz");
+            products.setProductNum(scan.nextInt());
+            if (products.getProductNum()<0 ||products.getProductNum()>10 ){
+                System.out.println("Gecerli bir product numarasi giriniz(1'den 10'a kadar)");
+            }
+        }while (products.getProductNum()<0 ||products.getProductNum()>10);
+        switch (products.getProductNum()){
+            case 0:
+                System.out.println("Tesekkur eder, tekrarbekleriz...");
+                break;
+            case 1:
+                products.setPrice(products.getSnicker());
+                break;
+            case 2:
+                products.setPrice(products.getChipsAhoy());
+                break;
+            case 3:
+                products.setPrice(products.getPringles());
+                break;
+            case 4:
+                products.setPrice(products.getMilano());
+                break;
+            case 5:
+                products.setPrice(products.getReeses());
+                break;
+            case 6:
+                products.setPrice(products.getCocaCola());
+                break;
+            case 7:
+                products.setPrice(products.getFanta());
+                break;
+            case 8:
+                products.setPrice(products.getWater());
+                break;
+            case 9:
+                products.setPrice(products.getArizona());
+                break;
+            case 10:
+                products.setPrice(products.getSkittles());
+                break;
+        }
+        return products.getPrice();
+    }
 
-        }while(product.getProductNum()<1||product.getProductNum()>10);
-        return 0;
+    public double balance(double price, double money, Products product){
+
+        while(money<price) {
+            Scanner scan= new Scanner(System.in);
+            System.out.println("Bakiye yetersiz, para eklemek istermisiniz? (Y/N)");
+            char onay=scan.next().toUpperCase().charAt(0);
+
+            if (onay=='N') {
+                System.out.println("Bakiyeniz "+money);
+                System.out.println("Tesekkur eder tekrarbekleriz...");
+                break;
+            } else if (onay=='Y') {
+                System.out.print("Ne kadar eklemek istersiniz: ");
+                money+=scan.nextDouble();
+                System.out.println("Hesabinzda " + money + " dolar var.");
+                price=select(product);
+            } else {
+                System.out.println("Lutfen 'Y' veya 'N' seciniz...");
+            }
+        }
+        return money;
+    }
+    public void purchase(double price,double money,Products product){
+        while(money>=product.getPrice()){
+            money=money-product.getPrice();
+            System.out.println("hesabinizda "+money+"dolar var");
+            Scanner scan=new Scanner(System.in);
+            System.out.println("alisverise devam etmek istermisiniz(y-n)");
+            char onay=scan.next().toUpperCase().charAt(0);
+
+            if (onay=='n'){
+                System.out.println("tesekkur eder, yine bekleriz");
+                break;
+
+            }else if(onay=='y'){
+                product.setPrice(select(product));
+                money=balance(product.getPrice(),money,product);
+            }
+        }
     }
 }
--- src/projectvendingmachine/Runner.java
+++ src/projectvendingmachine/Runner.java
@@ -6,10 +6,17 @@ public class Runner {
 
     public static void  main(String[] args) {
 
-        Products product =new Products();
 
-        Methods methods=new Methods();
+        Products product = new Products();
+
+        Methods methods = new Methods();
+        double money= methods.money();
+        System.out.println("Hesabinizda "+ money +" dollar var");
+        product.setPrice(methods.select(product));
         methods.select(product);
+        methods.balance(product.getPrice(),money,product);
+
+
     }
 
 }
